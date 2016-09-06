6383ddb0b39544b5ba5139d8ab4297fef25c30f5
k_kitamura <k_kitamura@monotaro.com>
2016-09-06 10:18:30 +0900

    fix threading end process

--- datadog/threadstats/periodic_timer.py
+++ datadog/threadstats/periodic_timer.py
@@ -35,6 +35,7 @@ class PeriodicTimer(Thread):
     def end(self):
         if self._is_alive():
             self.finished.set()
+            self.finished.join()
 
     def run(self):
         while True:
